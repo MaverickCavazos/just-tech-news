c6e98050c592f27c2bdde29cf057d14513a0c356
Maverick <Mav33987@gmail.com>
2022-02-03 23:07:40 -0600

    created post data model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations hasMany is a prebuilt fucntion/method of sequelize
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// belongsTo is a prebuild function/method of sequelize
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
